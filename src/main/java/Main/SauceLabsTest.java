4f645dc9fe0a45750f18cb22e73bb5485e36eeb7
INT\eduardo.diaz <eduardo.diaz@solera.com>
2021-02-22 13:15:19 +0100

    https

--- src/main/java/Main/SauceLabsTest.java
+++ src/main/java/Main/SauceLabsTest.java
@@ -27,6 +27,7 @@ public class SauceLabsTest {
         capabilities.setCapability("name", "SauceLabs Tunnel Test");
         capabilities.setCapability("idleTimeout", "90");
         capabilities.setCapability("newCommandTimeout", "90");
+        capabilities.setCapability("avoidProxy", true);
         String url = "https://" + SAUCE_USERNAME + ":" + SAUCE_USER_KEY + "@ondemand.eu-central-1.saucelabs.com:443" + "/wd/hub";
         System.out.println("[DEBUG][URL]: " + url);
 
