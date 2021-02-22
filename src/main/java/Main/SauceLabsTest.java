1fb3a0214fc54423b12beae0407fff965f2fa8f6
INT\eduardo.diaz <eduardo.diaz@solera.com>
2021-02-22 09:58:59 +0100

    New test

---
+++ src/main/java/Main/SauceLabsTest.java
@@ -0,0 +1,47 @@
+package Main;
+
+import static org.junit.Assert.assertTrue;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.By;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class SauceLabsTest {
+
+    private static RemoteWebDriver driver;
+
+    public static void main(String[] args) {
+
+        // SETUP
+        MutableCapabilities capabilities = new MutableCapabilities();
+        capabilities.setCapability("appiumVersion", "1.17.1");
+        capabilities.setCapability("platformName", "iOS");
+        capabilities.setCapability("platformVersion", "12.2");
+        capabilities.setCapability("deviceName", "iPhone 8");
+        capabilities.setCapability("browserName", "safari");
+        capabilities.setCapability("name", "SauceLabs Tunnel Test");
+        capabilities.setCapability("idleTimeout", "90");
+        capabilities.setCapability("newCommandTimeout", "90");
+
+        try {
+            driver = new RemoteWebDriver(
+                    new URL("https://" + System.getenv("SAUCE_USERNAME") + ":" +
+                            System.getenv("SAUCE_ACCESS_KEY") +
+                            "@ondemand.eu-central-1.saucelabs.com:443" + "/wd/hub"),
+                    capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        // TEST
+        driver.navigate().to("https://axn-testrail01-p.axadmin.net/index.php?/auth/login/");
+        assertTrue(driver.findElement(By.xpath("//h1[.='TestRail QA']")).isDisplayed());
+
+        // END
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
